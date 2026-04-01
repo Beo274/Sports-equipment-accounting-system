f9e4862319f2b0ae25bd4436a0832168e8f2cdf8
CyberBitsuha <edisonkms27s@mail.ru>
2026-04-01 20:46:20 +0300

    endpoint 8,9,10

--- src/main/java/ru/etu/sport/controller/ClassTreeController.java
+++ src/main/java/ru/etu/sport/controller/ClassTreeController.java
@@ -9,6 +9,7 @@ import ru.etu.sport.model.dto.response.ClassResponse;
 import ru.etu.sport.service.ClassTreeService;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,24 +24,24 @@ public class ClassTreeController {
     private final ClassTreeService classTreeService;
 
     @GetMapping("/{classId}/children")
-    public ResponseEntity<List<ClassHierarchyResponse>> getChildren(@PathVariable Integer classId) {
+    public ResponseEntity<Map<String, List<ClassHierarchyResponse>>> getChildren(@PathVariable Integer classId) {
         List<ClassHierarchyResponse> children = this.classTreeService.getChildren(classId);
-        log.info("Provided children");
-        return ResponseEntity.ok(children);
+        log.info("Provided children for classId: {}", classId);
+        return ResponseEntity.ok(Map.of("classes", children));
     }
-    
+
     @GetMapping("/{classId}/parents")
-    public ResponseEntity<List<ClassHierarchyResponse>> getParents(@PathVariable Integer classId) {
+    public ResponseEntity<Map<String, List<ClassHierarchyResponse>>> getParents(@PathVariable Integer classId) {
         List<ClassHierarchyResponse> parents = this.classTreeService.getParents(classId);
-        log.info("Provided parents");
-        return ResponseEntity.ok(parents);
+        log.info("Provided parents for classId: {}", classId);
+        return ResponseEntity.ok(Map.of("classes", parents));
     }
-    
+
     @GetMapping("/leaves")
-    public ResponseEntity<List<ClassResponse>> getLeaves() {
+    public ResponseEntity<Map<String, List<ClassResponse>>> getLeaves() {
         List<ClassResponse> leaves = this.classTreeService.getLeaves();
-        log.info("provided leaves");
-        return ResponseEntity.ok(leaves);
+        log.info("Provided leaves");
+        return ResponseEntity.ok(Map.of("classes", leaves));
     }
     
 }
