c15e9797463470eb1902abc137deb6667cfffe84
Beo274 <romka24.rk@gmail.com>
2026-05-06 07:41:31 +0000

    new: add entity and components

---
+++ src/main/java/ru/etu/sport/parameter/ParameterController.java
@@ -0,0 +1,8 @@
+package ru.etu.sport.parameter;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ParameterController {
+
+}
---
+++ src/main/java/ru/etu/sport/parameter/ParameterService.java
@@ -0,0 +1,4 @@
+package ru.etu.sport.parameter;
+
+public interface ParameterService {
+}
